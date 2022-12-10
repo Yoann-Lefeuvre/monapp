8f5609b6943c75c40f88bfb68e987392bf47e091
Yoann LEFEUVRE <yoann.lefeuvre@gmail.com>
2022-12-10 19:31:32 +0100

    Maintenance Modules

---
+++ src/app/face-snaps/face-snaps.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SingleFaceSnapComponent } from './components/single-face-snap/single-face-snap.component';
+import { NewFaceSnapComponent } from './components/new-face-snap/new-face-snap.component';
+import { FaceSnapComponent } from './components/face-snap/face-snap.component';
+import { FaceSnapListComponent } from './components/face-snap-list/face-snap-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    SingleFaceSnapComponent,
+    NewFaceSnapComponent,
+    FaceSnapComponent,
+    FaceSnapListComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
+  exports: [
+    SingleFaceSnapComponent,
+    NewFaceSnapComponent,
+    FaceSnapComponent,
+    FaceSnapListComponent,
+  ],
+})
+export class FaceSnapsModule {}
