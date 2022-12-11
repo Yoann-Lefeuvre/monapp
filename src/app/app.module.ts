9bcb7e5c1022469370cbb05cfa52642d4adc9aad
Yoann LEFEUVRE <yoann.lefeuvre@gmail.com>
2022-12-11 18:17:01 +0100

    Les Guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { LandingPageModule } from './landing-page/landing-page.module';
 import { FaceSnapsModule } from './face-snaps/face-snaps.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, LandingPageModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    LandingPageModule,
+    AuthModule,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
